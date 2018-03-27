aa2a642086e8036d80ef685943d3f9aee714532c
aom <aomany17@gmail.com>
2018-03-27 14:07:36 +0700

    Change 1 file

--- Lottery.c
+++ Lottery.c
@@ -7,7 +7,8 @@ void main()
   int i, won; 
   
   int YourLotteryNo;  
-   
+
+  printf("Hello Kitty");  
   printf("Enter your Lottery Number (0 to end): "); 
   scanf("%d", &YourLotteryNo); 
   while (YourLotteryNo != 0) 
